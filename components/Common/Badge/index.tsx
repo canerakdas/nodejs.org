0c24441a5966bf98cda610041f5abeac3089c1ef
Caner Akdas <canerakdas@gmail.com>
2023-09-11 18:59:41 +0300

    refactor: classname conditions moved into the CSS

--- components/Common/Badge/index.tsx
+++ components/Common/Badge/index.tsx
@@ -17,36 +17,18 @@ const Badge: FC<PropsWithChildren<BadgeProps>> = ({
   ...args
 }) => {
   if (typeof kind === 'undefined') kind = component.Default;
-  const iconClasses = classNames(styles.icon, {
-    [styles.iconWarning]: kind === component.Warning,
-    [styles.iconError]: kind === component.Error,
-    [styles.iconGreen]: kind === component.Default,
-  });
-
-  const wrapperClasses = classNames(styles.wrapper, {
-    [styles.wrapperWarning]: kind === component.Warning,
-    [styles.wrapperError]: kind === component.Error,
-    [styles.wrapperGreen]: kind === component.Default,
-  });
-
   const badgeClasses = classNames(styles.badge, {
     [styles.badgeWarning]: kind === component.Warning,
     [styles.badgeError]: kind === component.Error,
     [styles.badgeGreen]: kind === component.Default,
   });
 
-  const contentClasses = classNames(styles.content, {
-    [styles.contentWarning]: kind === component.Warning,
-    [styles.contentError]: kind === component.Error,
-    [styles.contentGreen]: kind === component.Default,
-  });
-
   return (
     <Link {...args}>
-      <div className={wrapperClasses}>
-        {badgeText && <span className={badgeClasses}>{badgeText}</span>}
-        <span className={contentClasses}>{children}</span>
-        <ArrowRightIcon className={iconClasses} />
+      <div className={badgeClasses}>
+        {badgeText && <span className={styles.text}>{badgeText}</span>}
+        <span className={styles.message}>{children}</span>
+        <ArrowRightIcon className={styles.icon} />
       </div>
     </Link>
   );
